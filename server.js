91f4e367b7ca5c980c106a0cfac34d6f38a0e47d
Arlindo Pinto <perensen@gmail.com>
2023-03-18 16:59:14 -0400

    Added Inquirer Prompt

---
+++ server.js
@@ -0,0 +1,12 @@
+//module.exports = { mainMenu };
+
+// Import Libraries
+//const inquirer = require('inquirer');
+//const mysql = require('mysql2');
+
+// Import Functions
+
+const {mainMenu} = require ('./lib/helpers/helper.js')
+
+
+mainMenu()
